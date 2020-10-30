c54f13f517afd12265b2ae1f2e393e70f51f712f
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-10-30 18:21:47 +0900

    get html from templates

--- 00-web-scrapper/web-scrapper/main.py
+++ 00-web-scrapper/web-scrapper/main.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScrapper")
 
 @app.route("/") #decorator : @--
 def home(): 
-  return "Hello Welcom to mi cassa!"
-# decorator 밑에는 반드시 함수가 와야한다.
+  return render_template("potato.html")
 
-@app.route("/<username>")
-def contact(username): #def의 이름은 contact이 아니어도 상관 없다.
-  return f"Hello {username} how are you doing"
+app.run(host = "0.0.0.0") 
 
-app.run(host = "0.0.0.0")
