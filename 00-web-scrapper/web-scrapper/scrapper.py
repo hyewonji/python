9e9d2a6907247dec8eef97a896f9aa910d9055ad
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-11-01 23:42:19 +0900

    add scrapper.py

---
+++ 00-web-scrapper/web-scrapper/scrapper.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_last_page(url):
+  result = requests.get(url)
+  soup = BeautifulSoup(result.text,"html.parser")
+  pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
+  last_page = pages[-2].get_text(strip = True)
+  return int(last_page)
+
+def extract_job(html):
+  title = html.find("h2").text.strip()
+  company, location = html.find("h3").find_all("span",recursive=False)# recursive=False : span안에 있는 span까지 가져오는것을 방지
+  company=company.get_text(strip=True)
+  location = location.get_text(strip=True)
+  job_id = html['data-jobid']
+  return {'title':title, 'company':company, 'location':location,
+  'apply_link': f"https://stackoverflow.com/jobs/{job_id}"}
+
+
+def extract_jobs(last_page, url):
+  jobs=[]
+  for page in range(last_page):
+    print(f"Scrapping 50: Page: {page}")
+    result = requests.get(f"{url}&pg={page+1}")
+    soup = BeautifulSoup(result.text,"html.parser")
+    results = soup.find_all("div",{"class":"-job"})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
+
+
+
+def get_jobs(word): 
+  url = f"https://stackoverflow.com/jobs?q={word}"
+  last_page = get_last_page(url)
+  jobs = extract_jobs(last_page,url)
+
+  return jobs
+
